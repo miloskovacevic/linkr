994c5cbe3a2ec9e4f5dd0f2d4b11cdc6f991fc68
Milos Kovacevic <miloscpt@gmail.com>
2015-09-29 03:25:25 +0200

    Like functionality works. Local storage problem fixed

---
+++ app/main.js
@@ -0,0 +1,66 @@
+//setovanje id-ja za novog korisnika ili uzimanje starog za starog korisika...
+function randomId(){
+    return Math.floor(Math.random() * 1e11);
+}
+var userId = localStorage.getItem("userId") || randomId();
+localStorage.setItem("userId", userId);
+console.log("Cao ja sam korisnik #" + userId);
+
+var socket = io.connect('http://localhost', {'forceNew':true});
+
+//kada se konektuje korisnik, saljemo serveru poruku o ID-ju...
+socket.emit("porukaIdKorisnika", userId);
+
+var messageCache;
+
+// server je poslao nizPorukaBrowseru paket koji parsiramo u stranicu...
+socket.on('nizPorukaBrowseru', function(data){
+    console.info(data);
+    //snimamo podatke za ubuduce...
+    messageCache = data;
+    render();
+});
+
+function render(){
+    var data = messageCache;
+    var html = data.map(function(data, index){
+        return (
+        '<form class="message" onsubmit="return likeMessage(messageCache[' + index + '])">' +
+        '<div class="name">' + data.userName + '</div> <a href="' + data.content.link + '" class="message" target=blank >' + data.content.text + '</a>' +
+        '<input type="submit" class="likes-count" value="' + data.likedBy.length + ' Likes">' +
+        '</form>'
+        );
+    }).join(" ");
+
+    document.getElementById("messages").innerHTML = html;
+
+}
+
+function likeMessage(message){
+    var index = message.likedBy.indexOf(userId);
+    if(index<0){
+        message.likedBy.push(userId);
+    }else {
+        message.likedBy.splice(index, 1);
+    }
+
+    socket.emit("update-message", message); // OVO DA SE ZAVRSI NA SERVERU
+    render();
+    return false;
+}
+
+function addMessage(e) {
+    var payload = {
+        messageId: randomId(),
+        userName: document.getElementById("username").value,
+        content: {
+                text: document.getElementById("message").value,
+                link: document.getElementById("linkAddress").value
+        },
+        likedBy: [],
+        ts: Date.now()
+    };
+    socket.emit("new-message", payload);
+    return false;
+}
+
--- server/main.js
+++ server/main.js
@@ -5,15 +5,90 @@ var server = require('http').Server(app);
 // deklarisemo i vezujemo socket.io za server...
 var io = require('socket.io')(server);
 
-//app.get('/', function(req, res){
-//    res.send('pusi karu!');
-//    console.log('something connected to express...');
-//});
+var messages = [
+    {
+        userId: 1,
+        messageId: 9,
+        userName: 'Milos Kovacevic',
+        content: {
+            text: 'Molderfcking crnnja in da haus!',
+            link: 'http://awoiaf.westeros.org/index.php/House_Stonetree'
+        },
+        likedBy:[4],
+        ts: Date.now() - 10000
+    },
+    {
+        userId: 2,
+        messageId: 11,
+        userName: 'Arya Stark',
+        content: {
+            text: 'We\'ll come see this inn.',
+            link: 'http://gameofthrones.wikia.com/wiki/Inn_at_the_Crossroads'
+        },
+        likedBy:[2,3,4],
+        ts: Date.now() - 10000
+    },
+    {
+        userId: 3,
+        messageId: 16,
+        userName: 'Cersei Lannister',
+        content: {
+            text: 'Her scheming forced this on me.',
+            link: 'http://gameofthrones.wikia.com/wiki/Margaery_Tyrell'
+        },
+        likedBy:[1],
+        ts: Date.now() - 10000
+    },
+    {
+        userId: 4,
+        messageId: 17,
+        userName: 'Sasa Djordjevic',
+        content: {
+            text: 'Najbolji srpski kosarkas ikada',
+            link: 'http://sr.wikipedia.org/sr/Александар_Ђорђевић_(кошаркаш)'
+        },
+        likedBy:[3],
+        ts: Date.now() - 10000
+    }
+];
+
 app.use(express.static('app'));
 
+//pri necijoj konekciji na server, u konzolu upisi notifikaciju i posalji niz poruka tom korisniku
 io.on('connection', function (socket) {
-    console.log("Something connected to Socket.io...");
-    socket.emit("messages",["Hello","Hi There", "How Are You?"]);
+
+    //server prima poruku od klijenta o uzetom id-ju...
+    socket.on("porukaIdKorisnika", function(podaci){
+        console.log("Konektovan korisnik sa id-jem #" + podaci);
+    });
+
+    //saljemo poruke browseru...
+    socket.emit("nizPorukaBrowseru", messages);
+
+    // a ukoliko nam korisnik preko browsera salje poruku, obradjujemo je...
+    socket.on('new-message', function (data) {
+        console.log(data.userName);
+        console.log(data.content.text);
+
+        //novu poruku saljemo u messages objekat i onda saljemo citav messages objekat browseru ponovo...
+        messages.push(data);
+
+        //ako ovako posaljemo , onda ce samo trenutni soket, odnosno browser da primi messages objekat i da se refreshuje
+        // socket.emit("nizPorukaBrowseru", messages);
+
+        // a ako uradimo ovako onda ce svaki soket koji je otvoren prema serveru da primi objekat i refreshuje browser
+        io.sockets.emit("nizPorukaBrowseru", messages);
+    });
+
+    socket.on("update-message", function (data) {
+        var message = messages.filter(function(message){
+            return message.messageId == data.messageId;
+        })[0];
+        message.likedBy = data.likedBy;
+        io.sockets.emit('nizPorukaBrowseru', messages);
+    });
+
+
 });
 
 
